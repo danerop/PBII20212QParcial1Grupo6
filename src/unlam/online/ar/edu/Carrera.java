8240b34982561dd1433bccd5a99dfb9a54f2eca2
danerop <danerop@hotmail.com>
2021-10-08 13:46:04 -0300

    correccion a carrera

--- src/unlam/online/ar/edu/Carrera.java
+++ src/unlam/online/ar/edu/Carrera.java
@@ -3,32 +3,20 @@ package unlam.online.ar.edu;
 public class Carrera extends Entrenamiento {
 
     private ZonasDeFrecuenciaCardiaca zonaDeFrecuenciaCardiaca;
-    private double distanciaRecorridaEnKm;
-    private double tiempoEnMin;
     private double ritmoXKm;
 
     public Carrera(Deportista deportista, Integer dia, Integer mes, Integer anio) {
         super(deportista, dia, mes, anio);
         this.zonaDeFrecuenciaCardiaca = new ZonasDeFrecuenciaCardiaca("Corredor");
-        this.distanciaRecorridaEnKm = 0.;
-        this.tiempoEnMin = 0;
         this.ritmoXKm = 0;
     }
 
     public void calcularRitmoXKm() {
-        this.ritmoXKm = this.tiempoEnMin / this.distanciaRecorridaEnKm;
+        this.ritmoXKm = ( (double)(super.getContadorSegundos()/60) ) / getDistanciaRecorrida();
     }
 
     public String obtenerZonaDeFrecuenciaCardiaca(int frecuenciaCardiaca) {
         return zonaDeFrecuenciaCardiaca.calcularZonaDeFrecuenciaCardiaca(frecuenciaCardiaca);
-    }
-
-    public double getDistanciaRecorridaEnKm() {
-        return distanciaRecorridaEnKm;
-    }
-
-    public double getTiempoEnMin() {
-        return tiempoEnMin;
     }
 
     public double getRitmoXKm() {
